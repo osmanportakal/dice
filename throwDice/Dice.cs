19683eab0f8300dc7e89fec91cbbddd316db5cbb
osmanportakal <gonyali_osman@hotmail.com>
2014-12-03 14:27:43 +0100

    first commit

---
+++ throwDice/Dice.cs
@@ -0,0 +1,47 @@
+﻿using Neo4jClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace throwDice
+{
+    class Dice
+    {
+        private GraphClient client;
+        
+       
+        public int num { get; set; }
+
+        public void initClientConnection()
+        {
+            try
+            {
+                if (this.client != null)
+                {
+                    return;
+                }
+
+                this.client = new GraphClient(new Uri("http://localhost:7474/db/data"));
+                this.client.Connect();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Could not make connection to database, check if database server is on and try again.");
+            }
+        }
+
+        public void createDice()
+        {
+            Console.Write(this.num);
+
+            this.client.Cypher
+                .Create("(dice:Dice {num:" + this.num + "})")
+                .WithParam("Num", this.num)
+                .ExecuteWithoutResults();
+        }
+
+
+    }
+}
---
+++ throwDice/Program.cs
@@ -0,0 +1,36 @@
+﻿using Neo4jClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace throwDice
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Dice dice1 = new Dice();
+            Dice dice2 = new Dice();
+
+            Dice testOne = new Dice();
+            Random random = new Random();
+            int roll1 = random.Next(1, 7);
+            int roll2 = random.Next(1, 7);
+            dice1.num = roll1;
+            dice2.num = roll2;
+
+            dice1.initClientConnection();
+            dice2.initClientConnection();
+            dice1.createDice();
+            dice2.createDice();
+
+        }
+
+       
+    }
+
+  
+}
